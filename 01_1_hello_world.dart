62cc85cf611409fb3b4eed039e3ca7293987af6c
Jayanta <bjayanta.me@gmail.com>
2022-12-05 15:26:59 +0600

    update repo

--- 01_1_hello_world.dart
+++
@@ -1,4 +0,0 @@
-void main() {
-  print("Hello world!");
-  print('jhasdkflj askjlfh asldkjf kj');
-}
--- 01_6_standard_input_output.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-void main() {
-  print("Enter your name: ");
-  String name = stdin.readLineSync()!;
-  print("Hi $name");
-  stdout.write("My name is $name");
-}
