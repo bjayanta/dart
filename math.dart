25dd4adb4d50f3634967eb193e621f596ba55d18
Jayanta <bjayanta.me@gmail.com>
2023-01-04 11:21:33 +0600

    add math

---
+++ math.dart
@@ -0,0 +1,13 @@
+/*
+Random number
+  - Get random number 0 - 9
+  - Get random number 1 - 10
+  - Generate random number between any number range
+  - Get random boolean value 
+  - Get random double Value
+  - Generate a list of 15 random numbers between 100 to 1000
+
+Creating a random number guessing game:
+  - 
+
+*/
