f4fca29a934198e8b7e38fcb9a1d592f44eb2894
Jayanta Biswas <bjayanta.me@gmail.com>
2023-01-04 12:52:59 +0600

    talk about required keyword

--- parameters_named.dart
+++ parameters_named.dart
@@ -4,10 +4,19 @@
 // 03. Set Default Value
 
 void main(List<String> args) {
-  fullname(first: "Joy", last: "dey");
-  fullname(last: "dey", first: "Joy", surname: "Mr.");
+  // fullname(first: "Joy", last: "dey");
+  // fullname(last: "dey", first: "Joy", surname: "Mr.");
+  // fullname();
+
+  bio(name: "Trishna");
 }
 
-void fullname({String? first, String? last, String? surname = "Sir/Mam"}) {
-  print("$surname $first $last");
+// void fullname({String? first, String? last, String? surname = "Sir/Mam"}) {
+//   print("$surname $first $last");
+// }
+
+void bio({required String name, String? height, String? phone = "n/a"}) {
+  print("My name is $name");
+  print("Height is $height");
+  print("Phone number is $phone");
 }
