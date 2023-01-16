a0411d180819fd356b9b53c2a4cde21ef1fca1d3
Jayanta <bjayanta.me@gmail.com>
2023-01-16 12:07:44 +0600

    update constant constructor

--- constructors/constant_constructor.dart
+++ constructors/constant_constructor.dart
@@ -1,7 +1,10 @@
-// Rule for declaring Constant Constructor
+// Constant Constructor:
+// Constant constructor is a constructor that creates a constant object. A constant object is an object whose value cannot be changed. A constant constructor is declared using the keyword const.
+
+// Rule for declaring Constant Constructor:
 // All properties of the class must be final
 // Does not have any body
-// Only class containing const constructor is initialized using the const keyword
+// Only class containing const constructor is initialized using the 'const' keyword
 
 // class Point {
 //   final int x;
@@ -17,6 +20,7 @@
 
 //   Point p2 = const Point(1, 2);
 //   print("The p2 hash code is: ${p2.hashCode}");
+
 //   // without using const
 //   // this has different hash code.
 //   Point p3 = Point(2, 2);
@@ -30,16 +34,17 @@
 class Car {
   final String? name;
   final String? model;
-  final int? prize;
+  final int? price;
 
   // Constant Constructor
-  const Car({this.name, this.model, this.prize});
+  const Car({this.name, this.model, this.price});
 }
 
 void main() {
-  // Here car is object of class Car.
-  const Car car = Car(name: "BMW", model: "X5", prize: 50000);
+  // Here 'car' is object of 'Car' class.
+  const Car car = Car(name: "BMW", model: "M85", price: 80000);
+
   print("Name: ${car.name}");
   print("Model: ${car.model}");
-  print("Prize: ${car.prize}");
+  print("Prize: ${car.price}");
 }
