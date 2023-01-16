0a57eb2df557124afe61f58fd38a94194af9a34d
Jayanta <bjayanta.me@gmail.com>
2023-01-16 11:38:41 +0600

    update default constructor file

---
+++ constructors/default_constructor.dart
@@ -0,0 +1,42 @@
+// Example:
+// class Car {
+//   String? brand;
+//   int? price;
+
+//   // Constructor
+//   Car() {
+//     print("This is a default constructor");
+//   }
+// }
+
+// void main() {
+//   // Here "car" is object of class "Car".
+//   Car car = Car();
+// }
+
+// Example:
+// class Student {
+//   String? name;
+//   int? age;
+//   String? institution;
+//   String? grade;
+
+//   // Default Constructor
+//   Student() {
+//     print("Constructor called"); // Check the constructor is called or not
+//     institution = "Mirtunjoy High School";
+//   }
+// }
+
+// void main() {
+//   Student student = Student(); // Here student is object of "Student" class
+
+//   student.name = "Jayanta Biswas";
+//   student.age = 165;
+//   student.grade = "A+";
+
+//   print("Name: ${student.name}");
+//   print("Age: ${student.age}");
+//   print("School Name: ${student.institution}");
+//   print("Grade: ${student.grade}");
+// }
